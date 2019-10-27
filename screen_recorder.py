db0b24c779973c0b0c9a1972cd5eb25c8448a34b
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-10-27 12:30:05 +0100

    Update screen_recorder.py

--- screen_recorder.py
+++ screen_recorder.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import pyaudio
 import cv2
 import ctypes
 import numpy as np
@@ -7,6 +8,7 @@ import pyautogui
 import threading
 
 recording = False
+num_frame=0
 
 def screen_s():
     #global user32
@@ -17,9 +19,17 @@ def screen_s():
 
 def init():
     global screen_size, fourcc, out
+    global audio, FORMAT, CHANNELS, RATE, outAudio
+    #VALORES PARA CAPTURA PANTALLA
     screen_size = screen_s()
     fourcc = cv2.VideoWriter_fourcc(*"XVID")
     out = cv2.VideoWriter("output.avi", fourcc, 20.0, (screen_size))
+    #VALORES PARA SONIDO
+    audio = pyaudio.PyAudio()
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 2
+    RATE = 44100
+    outAudio = "output.wav"
 
 def record_state():
     global recording
@@ -32,12 +42,14 @@ def record_state():
         t1.start()
 
 def record():
-    global img, frame, out
+    global img, frame, out, num_frame
     while recording == True:
         img = pyautogui.screenshot()
         frame = np.array(img)
         frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
         out.write(frame)
+        num_frame+=1
+        print(num_frame)
     recorder.configure(text="Grabar")
     out.release()
     
