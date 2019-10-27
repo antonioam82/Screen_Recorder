342284cdf347aa740ab5ab225b43b3ec4af9b353
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-10-27 15:58:55 +0100

    Create screen_recorder0.py

---
+++ screen_recorder0.py
@@ -0,0 +1,53 @@
+from tkinter import *
+import cv2
+import ctypes
+import numpy as np
+from time import perf_counter
+import pyautogui
+import threading
+
+recording = False
+
+def screen_s():
+    #global user32
+    user32 = ctypes.windll.user32
+    user32.SetProcessDPIAware()
+    dimensions = user32.GetSystemMetrics(0),user32.GetSystemMetrics(1)
+    return dimensions
+
+def init():
+    global screen_size, fourcc, out
+    screen_size = screen_s()
+    fourcc = cv2.VideoWriter_fourcc(*"XVID")
+    out = cv2.VideoWriter("output.avi", fourcc, 20.0, (screen_size))
+
+def record_state():
+    global recording
+    if recording == True:
+        recording = False
+    else:
+        recording = True
+        recorder.configure(text="Parar")
+        t1=threading.Thread(target=record)
+        t1.start()
+
+def record():
+    global recording, out
+    while recording == True:
+        img = pyautogui.screenshot()
+        frame = np.array(img)
+        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+        out.write(frame)
+    recorder.configure(text="Grabar")
+    out.release()
+    recording = False
+
+ventana = Tk()
+ventana.geometry("150x80")
+ventana.title("Screen Rescorder")
+init()
+
+recorder = Button(ventana,text="Grabar",command=record_state)
+recorder.pack(padx=10,pady=20)
+
+ventana.mainloop()
