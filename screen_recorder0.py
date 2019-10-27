2dbecfee26d12fa204fddabea7e93920ba40d067
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-10-27 17:38:19 +0100

    Update screen_recorder0.py

--- screen_recorder0.py
+++ screen_recorder0.py
@@ -16,21 +16,26 @@ def screen_s():
     return dimensions
 
 def record_state():
+    global out
     global recording
     if recording == True:
         recording = False
     else:
         recording = True
+        out = cv2.VideoWriter("output.avi", fourcc, 20.0, (screen_size))
         recorder.configure(text="Parar")
         t1=threading.Thread(target=record)
         t1.start()
 
 def record():
+    global out
     while recording == True:
+        print("GRABANDO")
         img = pyautogui.screenshot()
         frame = np.array(img)
         frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
         out.write(frame)
+    print("FIN")
     recorder.configure(text="Grabar")
     out.release()
 
@@ -38,7 +43,7 @@ ventana = Tk()
 ventana.geometry("150x80")
 ventana.title("Screen Rescorder")
 screen_size = screen_s()
-out = cv2.VideoWriter("output.avi", fourcc, 20.0, (screen_size))
+#out = cv2.VideoWriter("output.avi", fourcc, 20.0, (screen_size))
 
 recorder = Button(ventana,text="Grabar",command=record_state)
 recorder.pack(padx=10,pady=20)
